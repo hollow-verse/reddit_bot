a92ee8025fe0c502afc3bf06417c2759e4a76010
laraib_sidd <laraib.sidd7@gmail.com>
2023-11-11 22:08:43 +0530

    s

--- lib/test.py
+++ lib/test.py
@@ -6,15 +6,15 @@ import os
 # Define your constants
 TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID =  os.environ.get('TELEGRAM_CHAT_ID')
-VALID_FLIAR_TEXT = os.environ.get('VALID_FLIAR_TEXT')
+
 
 def main():
+    VALID_FLIAR_TEXT = os.environ.get('VALID_FLIAR_TEXT').split(',')
+    sub_names = os.getenv('SUB_NAMES', '[]').split(',')
     # Initialize the Telegram Bot
+    print(sub_names)
     print(VALID_FLIAR_TEXT)
-    valid_flair_texts = VALID_FLIAR_TEXT.split(',')
-
-    if 'Task' in valid_flair_texts:
-        print('Task is present')
+   
 
 if __name__ == "__main__":
     main()
