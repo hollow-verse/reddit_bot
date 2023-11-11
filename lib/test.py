f1c71f249fb134fb357665f7cd2bc53a63133215
laraib_sidd <laraib.sidd7@gmail.com>
2023-11-11 16:30:33 +0530

    test

--- lib/test.py
+++ lib/test.py
@@ -1,12 +1,10 @@
 import os
 
-var = os.getenv('MONGO_USER')
-print(var)
-
 # Description: This file contains the Telegram Bot logic
 from telegram import Bot
 from utils import get_all_posts
 import os
+sub_names = os.getenv('SUB_NAMES')
 
 # Define your constants
 TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
@@ -15,7 +13,7 @@ TELEGRAM_CHAT_ID =  os.environ.get('TELEGRAM_CHAT_ID')
 def main():
     # Initialize the Telegram Bot
     bot = Bot(token=TELEGRAM_TOKEN)
-    bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=f"test {var} dsadsa")
+    bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=f"test {sub_names} dsadsa")
 
 
 if __name__ == "__main__":
