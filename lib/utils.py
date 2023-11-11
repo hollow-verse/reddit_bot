66703bdfd3ed58757de20a6e822b6fee506038bd
laraib_sidd <laraib.sidd7@gmail.com>
2023-11-11 21:50:35 +0530

    test

--- lib/utils.py
+++ lib/utils.py
@@ -4,9 +4,6 @@ import pymongo
 import os
 import praw
 
-# Firebase
-
-
 def get_reddit_client():
     reddit = praw.Reddit(
         client_id=os.environ.get("REDDIT_CLIENT_ID"),
@@ -24,8 +21,6 @@ def create_mongo_client():
     mongo_db = os.environ.get("MONGO_DB_NAME")
     srv = f"mongodb+srv://{mongo_user}:{mongo_pass}@{mongo_uri}/?retryWrites=true&w=majority"
     client = pymongo.MongoClient(srv)
-    print(mongo_db)
-    print(type(mongo_db))
     return client[mongo_db]
 
 
