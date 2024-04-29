7a3cac041629921fc9b63580e1ab7edf46032c07
laraib_sidd <laraib.sidd7@gmail.com>
2024-04-29 14:05:02 +0530

    Refactor bot.py to use asyncio for sending posts to Telegram

--- lib/bot.py
+++ lib/bot.py
@@ -2,10 +2,13 @@
 from telegram import Bot
 from utils import get_all_posts
 import os
+import asyncio
 
 # Define your constants
 TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID =  os.environ.get('TELEGRAM_CHAT_ID')
+
+
 
 def main():
     # Initialize the Telegram Bot
@@ -14,12 +17,19 @@ def main():
     # Get filtered subreddit posts
     filtered_sub_posts = get_all_posts()
 
+    # Create a new event loop
+    loop = asyncio.get_event_loop()
+
     # Loop through the posts and send each one to Telegram
     for posts in filtered_sub_posts:
         for post in posts:
-            send_post_to_telegram(bot, post, TELEGRAM_CHAT_ID)
+            # Schedule the send_post_to_telegram coroutine to run on the event loop
+            loop.run_until_complete(send_post_to_telegram(bot, post, TELEGRAM_CHAT_ID))
 
-def send_post_to_telegram(bot, post, chat_id):
+    # Close the event loop
+    loop.close()
+
+async def send_post_to_telegram(bot, post, chat_id):
     subreddit = post.get('subreddit', 'Null')
     title = post.get('title', 'Null')
     posted_ago = f"{post.get('posted_ago', 'Null')} hours"
@@ -34,7 +44,7 @@ def send_post_to_telegram(bot, post, chat_id):
         Text: {text}
     """
     
-    bot.send_message(chat_id=chat_id, text=msg)
+    await bot.send_message(chat_id=chat_id, text=msg)
 
 if __name__ == "__main__":
     main()
